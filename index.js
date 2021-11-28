f4ac0211ca13f7d4e2ef648036e7cb7c4a94c88d
Vaibhav Arora <varora1406@gmail.com>
2021-11-28 21:20:42 +0530

    feat: 🎸 add code for random user pickup

--- index.js
+++ index.js
@@ -9,16 +9,32 @@ const bot = new App({
 (async () => {
     // Start the app
     await bot.start(process.env.PORT || 3000);
-
     console.log('⚡️ Bolt app is running!');
 
-    bot.event("app_mention", async ({ event, say }) => {
-        try {
-            console.log(event, say)
-            await say(`Hey yoo <@${event.user}> you mentioned me`);
-        }
-        catch (e) {
-            console.log(`error responding ${e}`);
-        }
+    bot.command('/pickup', async ({ command, ack, say }) => {
+        const membersResponse = await bot.client.conversations.members({ channel: command.channel_id });
+
+        // Acknowledge command request
+        await ack();
+        const realUsers = await filterRealUsers(bot, membersResponse.members)
+        const userChoosenId = getRandom(realUsers).id;
+
+        await say(`Hey <@${userChoosenId}> , you are the picked up randomly`);
     });
 })();
+
+async function filterRealUsers(bot, usersID) {
+    const usersResponse = await Promise.allSettled(usersID.map(id =>
+        bot.client.users.info({ user: id })
+    ))
+
+    const isRealUser = (user) => !user.is_bot;
+    const users = usersResponse.map(response => response.value.user)
+    const realUsers = users.filter(isRealUser);
+
+    return realUsers
+}
+
+function getRandom(list) {
+    return list[Math.floor((Math.random() * list.length))];
+}
